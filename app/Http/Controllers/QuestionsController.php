f057fb25e51ff9fb89bc479234a75ab1ba514a6e
Swieckowski <a.swieckowski@gmail.com>
2018-03-01 16:55:01 -0500

    https fix attempt

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Question;
+use DB;
 
 class QuestionsController extends Controller
 {
     public function index()
     {   
-        $questions = Question::all();
+        $questions = DB::table('questions')::all();
         return response()->json($questions, 200);
     }
 }
